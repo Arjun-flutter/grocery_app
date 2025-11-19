4618075a5557115d076032cf183f8a173abb159d
arjun_flutter <nagarjunareddyavula1419@gmail.com>
2025-11-19 17:38:24 +0530

    Update code

--- lib/data/fake_db.dart
+++ lib/data/fake_db.dart
@@ -5,9 +5,9 @@ class FakeDB {
     {"name": "Eggs", "price": 60, "image": "🥚", "cat": "Dairy"},
     {"name": "Apples", "price": 120, "image": "🍎", "cat": "Fruits"},
     {"name": "Banana", "price": 40, "image": "🍌", "cat": "Fruits"},
-    {"name": "Carrot", "price": 50, "image": "🥕", "cat": "Fruits"},
-    {"name": "Lemons", "price": 50, "image": "🍋", "cat": "Fruits"},
-    {"name": "Onion", "price": 50, "image": "🧅", "cat": "Fruits"},
+    {"name": "Carrot", "price": 50, "image": "🥕", "cat": "Vegetables"},
+    {"name": "Lemons", "price": 50, "image": "🍋", "cat": "Vegetables"},
+    {"name": "Onion", "price": 50, "image": "🧅", "cat": "Vegetables"},
     {"name": "Mango", "price": 90, "image": "🥭", "cat": "Fruits"},
   ];
 }
